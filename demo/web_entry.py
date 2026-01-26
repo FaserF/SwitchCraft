28d3ea989f97c24a8424225ddfc71f10ff136723
FaserF <seitzf1@yahoo.de>
2026-01-26 09:14:01 +0000

    Deploying to gh-pages from @ FaserF/SwitchCraft@0aa0410d4457b54a9d08cf20af18abd97d641868 🚀

--- demo/web_entry.py
+++ demo/web_entry.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 print("DEBUG: WEB ENTRY RELOADED")
-print(f"BUILD_TIME: Mon Jan 26 08:00:42 UTC 2026")
+print(f"BUILD_TIME: Mon Jan 26 09:13:27 UTC 2026")
 
 # ============================================================
 # CRITICAL: Patch ssl module BEFORE any urllib3 import
